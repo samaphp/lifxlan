cd021be3d094525b6120e982e0da3eeabeabb9c2
samaphp <samaphp@gmail.com>
2019-09-22 06:29:13 +0300

    Add example file.

---
+++ examples/example.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Samaphp\LifxLan\Message\Light;
+
+// Example 1.
+// Setting color by custom values.
+// Insert your light IP.
+$light_ip = '192.168.1.175';
+$Light = new Light($light_ip);
+// Setting a custom color.
+// Max: 65535.
+$hue = 25555;
+// Max: 65535.
+$saturation = 55555;
+// Max: 65535.
+$bright = 5000;
+// Range 2500 to 9000.
+$kelvin = 9000;
+// $Light->setColor($hue, $saturation, $bright, $kelvin);
+
+// Example 2.
+// Setting the color by name.
+// Insert your light IP.
+$light_ip = '192.168.1.169';
+$Light = new Light($light_ip);
+$Light->setColorByName('white_warm', 40);
+
+// List of pre-defined colors by this class to use with setColorByName().
+print_r(array_keys($Light->colorNames()));
