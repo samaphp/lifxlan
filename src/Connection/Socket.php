f05b9f3456c468911d74cb143b54f00e9a93289f
samaphp <samaphp@gmail.com>
2019-11-09 07:16:20 +0300

    Enhance Socket class to allow for change the timeout value.

--- src/Connection/Socket.php
+++ src/Connection/Socket.php
@@ -4,12 +4,19 @@ namespace Samaphp\LifxLan\Connection;
 
 class Socket implements ConnectionInterface {
 
+  public $timeout;
+
+  public function __construct($timeout = 3)
+  {
+    $this->timeout = $timeout;
+  }
+
   public function connect($targeted_ip = FALSE, $data = NULL, $targeted_port = 56700) {
     if (filter_var($targeted_ip, FILTER_VALIDATE_IP)) {
       $host = sprintf('udp://%s', $targeted_ip);
       $socket = fsockopen($host, $targeted_port, $errno, $errstr);
       fwrite($socket, $data);
-      stream_set_timeout($socket, 3, 1);
+      stream_set_timeout($socket, $this->timeout, 1);
       $data = fgets($socket);
       fclose($socket);
       return $data;
