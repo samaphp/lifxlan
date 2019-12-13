abd62253bf69170504350338e3d2f49223e6a80c
samaphp <samaphp@gmail.com>
2019-12-14 00:38:32 +0300

    Implement setTimeout to allow override the default timeout value.

--- src/LifxLanClass.php
+++ src/LifxLanClass.php
@@ -10,10 +10,16 @@ Class LifxLanClass {
    * @var \Samaphp\LifxLan\Connection\ConnectionInterface
    */
   protected $driver;
+  private $timeout = 3;
 
   public function __construct($driver = NULL)
   {
     $this->driver = $driver ?: new Socket();
+  }
+
+  public function setTimeout($timeout) {
+    $this->timeout = $timeout;
+    return $this;
   }
 
   public static function discover() {
@@ -35,7 +41,7 @@ Class LifxLanClass {
 
   public function connect($targeted_ip, $data) {
     $driver = $this->getDriver();
-    return $driver->connect($targeted_ip, $data);
+    return $driver->setTimeout($this->timeout)->connect($targeted_ip, $data);
   }
 
   public function getDriver() {
