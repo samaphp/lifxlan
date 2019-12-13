df639a068f6576784924ff860a3510256c79a7de
samaphp <samaphp@gmail.com>
2019-12-14 00:30:38 +0300

    Enhance the Socket class and add new set function for timeout value.

--- src/Connection/Socket.php
+++ src/Connection/Socket.php
@@ -4,13 +4,29 @@ namespace Samaphp\LifxLan\Connection;
 
 class Socket implements ConnectionInterface {
 
-  public $timeout;
+  private $timeout = 3;
 
-  public function __construct($timeout = 3)
-  {
+  /**
+   * Setting the connection timeout.
+   *
+   * @param $timeout
+   *
+   * @return $this
+   */
+  public function setTimeout($timeout) {
     $this->timeout = $timeout;
+    return $this;
   }
 
+  /**
+   * Broadcasting the message.
+   *
+   * @param bool $targeted_ip
+   * @param null $data
+   * @param int $targeted_port
+   *
+   * @return bool|string|void|null
+   */
   public function connect($targeted_ip = FALSE, $data = NULL, $targeted_port = 56700) {
     if (filter_var($targeted_ip, FILTER_VALIDATE_IP)) {
       $host = sprintf('udp://%s', $targeted_ip);
